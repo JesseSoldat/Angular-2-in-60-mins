8c83ecf1b3632a3f3f4772359b467d12c56f24ac
jesse <jsoldat@hotmail.com>
2017-01-01 14:45:16 +0700

    can show and hide hobbies

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -3,17 +3,53 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'user',
 	template: `
-	<h3>Hello {{name}}</h3>
+	<h2>Hello {{name}}</h2>
 	<p><strong>Email:</strong> {{email}}</p>
 	<p><strong>Address:</strong> {{address.street}} {{address.city}} {{address.country}}</p>
+	<button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+	<div *ngIf="showHobbies">
+		<h3>Hobbies</h3>
+		<ul>
+			<li *ngFor="let hobby of hobbies">{{hobby}}</li>
+		</ul>
+	</div>
+	
 	`
 })
 export class UserComponent {
-	name = 'Jesse Soldat';
-	email = 'jlab@jlab.com';
-	address = { 
-		street: '775 Twin Dr.',
-		city: 'Bangkok',
-		country: 'Thailand'
+	name: string;
+	email: string;
+	address: address;
+	hobbies: string[];
+	showHobbies: boolean;
+	toggleText: string;
+
+	constructor(){
+		this.name = 'Jesse Soldat';
+		this.email = 'jlab@jlab.com';
+		this.address = { 
+			street: '775 Twin Dr.',
+			city: 'Bangkok',
+			country: 'Thailand'
+		}
+		this.hobbies = ['coding', 'manga', 'travel'];
+		this.showHobbies = false;
+	}
+
+	toggleHobbies(){
+		console.log('toggleHobbies')
+		if(this.showHobbies === true){
+			this.showHobbies = false;
+
+		} else {
+			this.showHobbies = true;
+
+		}
 	}
 }
+	
+	interface address {
+		street: string;
+		city: string;
+		state: string;
+	}
